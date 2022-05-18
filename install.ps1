ffe55c500ddd5111edf6b35beb7ca4947ca61739
Luke Coleman <41374939+luke-coleman@users.noreply.github.com>
2022-05-18 09:51:18 +0100

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,3 @@
+winget install -e --id Microsoft.VisualStudioCode --accept-package-agreements --accept-source-agreements
+winget install -e --id Valve.Steam --accept-package-agreements --accept-source-agreements
+winget install -e --id OpenJS.NodeJS --accept-package-agreements --accept-source-agreements
